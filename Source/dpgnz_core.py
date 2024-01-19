72b857007d1363f20a177c7d4efb98fd82f77fb6
Leoioz <73148445+Leoioz@users.noreply.github.com>
2024-01-19 11:29:09 +0800

    update

--- Source/dpgnz_core.py
+++ Source/dpgnz_core.py
@@ -31,11 +31,15 @@ class mywgeit(Ui_Form):
         super().__init__()
         
 if  __name__ == "__main__" :
-    app = QApplication([])
-    window = QWidget()
     
-    ui =mywgeit()
-    ui.setupUi(window)
+    app = QApplication()#这句是为了新建一个窗口app
     
+    window = QWidget()#这句是为了初始化咱们在pyside6里面的窗口，就是对象检查器的那个类
+    
+    ui =Ui_Form()#这句是用我们自动生成的UI的类，生成一个实例
+    # ui =mywgeit()
+    
+    ui.setupUi(window)#setupUi是UIform的函数，负责设置窗口的UI布局和组件，材料是QWidget类的实例
+    #单步调试下来发现，其实widget是根本的窗口类，我们创建一个实例，有窗口得告诉窗口里面什么东西放哪里，而放的内容描述清单就是designer里面uic转出的py文件
     window.show()
     app.exec_()
