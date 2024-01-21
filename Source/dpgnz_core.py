069d0565c99a9e1e74e1227492e5df1a513a0ca7
zhencw <82295945+zhencw@users.noreply.github.com>
2024-01-21 10:11:58 +0800

    123

--- Source/dpgnz_core.py
+++ Source/dpgnz_core.py
@@ -22,31 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-import os
-import sys
-import time
-sys.path.append('..\\dpgnz\\ui')
-from dpgnz_data import *
-from dpgnz_ui import *
-
-class mywgeit(Ui_MainWindow):
-    def __init__ (self):
-        super().__init__()
-        
-if  __name__ == "__main__" :
-    
-    app = QApplication()#这句是为了新建一个窗口app
-    
-    window = QMainWindow()#这句是为了初始化咱们在pyside6里面的窗口，就是对象检查器的那个类
-    
-    ui =Ui_MainWindow()#这句是用我们自动生成的UI的类，生成一个实例
-    # ui =mywgeit()
-    
-    ui.setupUi(window)#setupUi是UIform的函数，负责设置窗口的UI布局和组件，材料是QWidget类的实例
-    #单步调试下来发现，其实widget是根本的窗口类，我们创建一个实例，有窗口得告诉窗口里面什么东西放哪里，而放的内容描述清单就是designer里面uic转出的py文件，setupui这个函数作用是把uifrom里面描述的布局内容，放置到qwidget的窗口中。
-    window.show()
-    app.exec_()
-    
 # 111
 # 32656654654
 123
